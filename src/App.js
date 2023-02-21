8dd9eb58332e6d8f1fce0a8d0450e1113ab76272
Rifqi Subagja <rifqisubagjaa@gmail.com>
2023-02-21 11:37:31 +0700

    describe code

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import unsplash from "./unsplash";
 import ListImages from "./ListImages";
 
 class App extends React.Component {
+  // penyimpanan data gambar
   state = { images: [] };
 
   onSearchSubmit = async (term) => {
     const res = await unsplash.get("/search/photos", {
+      // ambil data dari unsplash berdasarkan term/inputan user
       params: { query: term },
     });
 
@@ -19,7 +21,9 @@ class App extends React.Component {
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
+        {/* get term from search bar */}
         <SearchBar onSubmit={this.onSearchSubmit} />
+        {/* passing image data to show it */}
         <ListImages data={this.state.images} />
       </div>
     );
